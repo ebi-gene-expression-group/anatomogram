725fa65a32843a379b037b8f9c044e79f737a1f1
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-21 04:07:28 +0100

    Resolve paths with Url.resolve to allow to point to a different server to download resources

--- src/imagesAvailable.js
+++ src/imagesAvailable.js
@@ -1,36 +1,37 @@
+const Url = require(`url`);
 const Path = require(`path`);
 const SvgsForSpecies = require(`../assets/json/svgsForSpecies.json`);
 const IdsForSvgs = require(`../assets/json/idsForSvgs.json`);
 
 const ResolvePathToIcon = (pathToFolderWithBundledResources, type, selected) =>
-  Path.resolve(
-      pathToFolderWithBundledResources,
-      Path.basename(require(`../assets/icons/${type}_${selected ? `` : `un`}selected.png`))
-  );
+    Url.resolve(
+        pathToFolderWithBundledResources,
+        Path.basename(require(`../assets/icons/${type}_${selected ? `` : `un`}selected.png`))
+    );
 
 const ResolvePathToSvg = (pathToFolderWithBundledResources, svg) =>
-    Path.resolve(
+    Url.resolve(
         pathToFolderWithBundledResources,
         Path.basename(require(`../assets/svg/${svg}`))
     );
 
 const GetSvgsForSpecies = (pathToFolderWithBundledResources, species) => {
-  let svgEntry = SvgsForSpecies[species];
-  if (typeof svgEntry === `object`) {
-    return Object.keys(svgEntry).map(anatomogramType => (
-        {
-          type: anatomogramType,
-          path: ResolvePathToSvg(pathToFolderWithBundledResources, svgEntry[anatomogramType]),
-          ids: IdsForSvgs[svgEntry[anatomogramType]]
-        }
-    ));
-  } else if (typeof svgEntry === `string`) {
-    return [{
-      type: `svg`,
-      path: ResolvePathToSvg(pathToFolderWithBundledResources, svgEntry),
-      ids: IdsForSvgs[svgEntry]
-    }];
-  }
+    let svgEntry = SvgsForSpecies[species];
+    if (typeof svgEntry === `object`) {
+        return Object.keys(svgEntry).map(anatomogramType => (
+            {
+                type: anatomogramType,
+                path: ResolvePathToSvg(pathToFolderWithBundledResources, svgEntry[anatomogramType]),
+                ids: IdsForSvgs[svgEntry[anatomogramType]]
+            }
+        ));
+    } else if (typeof svgEntry === `string`) {
+        return [{
+            type: `svg`,
+            path: ResolvePathToSvg(pathToFolderWithBundledResources, svgEntry),
+            ids: IdsForSvgs[svgEntry]
+        }];
+    }
 };
 
 module.exports = {GetSvgsForSpecies, ResolvePathToIcon};
