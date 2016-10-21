e29ecb2686a0bea35797fa526a7bc5793dc93cf7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-21 04:09:07 +0100

    Fix location of bundles

--- html/demo.js
+++ html/demo.js
@@ -7,9 +7,6 @@ var ReactDOM = require('react-dom');
 
 //*------------------------------------------------------------------*
 
-
-//*------------------------------------------------------------------*
-
 module.exports = function(mountNode) {
   ReactDOM.render(
       React.createElement(require('./Demo.jsx'), {}
