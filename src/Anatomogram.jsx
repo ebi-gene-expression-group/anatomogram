79de9a992e3390049dfb3c3a15d4712c9d7f78d7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-27 17:47:54 +0100

    ES6-ify

--- src/Anatomogram.jsx
+++ src/Anatomogram.jsx
@@ -1,25 +1,18 @@
-"use strict";
+const React = require(`react`);
+const AnatomogramImage = require(`./AnatomogramImage.jsx`);
+const SelectionIcon = require(`./SelectionIcon.jsx`);
 
-//*------------------------------------------------------------------*
-
-var React = require('react');
-
-var AnatomogramImage = require('./AnatomogramImage.jsx');
-var SelectionIcon = require('./SelectionIcon.jsx');
-
-//*------------------------------------------------------------------*
-
-var Anatomogram = React.createClass({
+const Anatomogram = React.createClass({
     propTypes: {
         pathToFolderWithBundledResources: React.PropTypes.string.isRequired,
         expressedTissueColour: React.PropTypes.string.isRequired,
         hoveredTissueColour: React.PropTypes.string.isRequired,
         availableAnatomograms : React.PropTypes.arrayOf(
-          React.PropTypes.shape({
-            type:React.PropTypes.string.isRequired,
-            path:React.PropTypes.string.isRequired,
-            ids: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
-          })
+            React.PropTypes.shape({
+                type:React.PropTypes.string.isRequired,
+                path:React.PropTypes.string.isRequired,
+                ids: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
+            })
         ).isRequired,
         height: React.PropTypes.number.isRequired,
         whenMousedOverIdsChange: React.PropTypes.func,
@@ -27,74 +20,62 @@ var Anatomogram = React.createClass({
         idsToBeHighlighted: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
     },
 
-    getInitialState: function() {
-        return {
-          selectedType: this.props.availableAnatomograms[0].type,
-        };
+    getInitialState() {
+        return { selectedType: this.props.availableAnatomograms[0].type };
     },
 
-    render: function () {
+    render() {
         return (
-            <div className="gxaAnatomogram" style={{display: "table", paddingTop: "4px"}}>
-                <div style={{display: "table-row"}}>
-                    <div style={{display: "table-cell", verticalAlign: "top"}}>
-                      {this._anatomogramSelectImageButtons()}
+            <div className="gxaAnatomogram" style={{display: `table`, paddingTop: `4px`}}>
+                <div style={{display: `table-row`}}>
+                    <div style={{display: `table-cell`, verticalAlign: `top`}}>
+                        {this._anatomogramSelectImageButtons()}
                     </div>
                     <AnatomogramImage
-                      key={this.state.selectedType}
-                      ref="currentImage"
-                      file={this._selectedAnatomogram().path}
-                      allSvgPathIds={this.props.allSvgPathIds || this._selectedAnatomogram().ids}
-                      {...this.props} />
+                        key={this.state.selectedType}
+                        file={this._selectedAnatomogram().path}
+                        allSvgPathIds={this.props.allSvgPathIds || this._selectedAnatomogram().ids}
+                        {...this.props} />
                 </div>
             </div>
         );
     },
 
-    _anatomogramSelectImageButtons : function(){
-      return (
-        this.props.availableAnatomograms.length < 2
-        ? []
-        : this.props.availableAnatomograms
-          .map(function(availableAnatomogram) {
-             return(
-                 <SelectionIcon
-                  key={availableAnatomogram.type + "_toggle"}
-                  pathToFolderWithBundledResources={this.props.pathToFolderWithBundledResources}
-                  anatomogramType={availableAnatomogram.type}
-                  selected={this.state.selectedType === availableAnatomogram.type}
-                  onClick={function(){this._afterUserSelectedAnatomogram(availableAnatomogram.type);}.bind(this)}/>
-             )
-          }.bind(this))
-      );
+    _anatomogramSelectImageButtons() {
+        return (
+            this.props.availableAnatomograms.length < 2 ?
+                [] :
+                this.props.availableAnatomograms
+                .map(availableAnatomogram =>
+                    (
+                        <SelectionIcon
+                            key={`${availableAnatomogram.type}_toggle`}
+                            pathToFolderWithBundledResources={this.props.pathToFolderWithBundledResources}
+                            anatomogramType={availableAnatomogram.type}
+                            selected={this.state.selectedType === availableAnatomogram.type}
+                            onClick={() => { this._afterUserSelectedAnatomogram(availableAnatomogram.type); }}/>
+                    )
+                )
+        );
     },
 
-    _afterUserSelectedAnatomogram: function(newSelectedType) {
+    _afterUserSelectedAnatomogram(newSelectedType) {
         if (newSelectedType !== this.state.selectedType) {
-            this.setState({selectedType: newSelectedType});
+            this.setState({ selectedType: newSelectedType });
         }
     },
 
-    _selectedAnatomogram: function() {
-      var type = this.state.selectedType;
-      return (
-        this.props.availableAnatomograms
-        .filter(function(e,ix){
-          return (
-            e.type === type
-          )
-        })
-        .concat({
-          type:"_",
-          path:"__invalid__.svg",
-          ids: []
-        })
-        [0]
-      );
+    _selectedAnatomogram() {
+        return (
+            this.props.availableAnatomograms
+            .filter(e => e.type === this.state.selectedType)
+            .concat({
+                type: `_`,
+                path: `__invalid__.svg`,
+                ids: []
+            })[0]
+        );
     },
-
 });
-
-//*------------------------------------------------------------------*
 
 module.exports = Anatomogram;
