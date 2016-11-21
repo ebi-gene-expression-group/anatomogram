31a659f08e694e4ea0d79fb08adb3778d291f168
wbazant <wbazant@ebi.ac.uk>
2016-11-21 12:15:46 +0000

    the script only picks up the files with .svg ending

--- scripts/idsForSvgs.py
+++ scripts/idsForSvgs.py
@@ -11,20 +11,17 @@ SVG_DIR=SCRIPT_DIR+'/../resources/svg'
 TARGET=SCRIPT_DIR+'/../resources/json/idsForSvgs.json'
 print("Regenerating the file %s"%TARGET)
 svgsFound = []
-regex = "\.DS_Store$"
 for (dirpath, dirnames, filenames) in os.walk(SVG_DIR):
     for filename in filenames:
-        if re.match(regex, filename):
-		continue
-	else: 
-		svgsFound.append(os.path.join(dirpath, filename))
+        if re.match(r".*\.svg", filename):
+	    svgsFound.append((dirpath, filename))
     break
-def idsInFile(filename):
-    print("Parsing: "+filename)
-    e = xml.etree.ElementTree.parse(filename).getroot()
+def idsInFile(filepath):
+    print("Parsing: "+filepath)
+    e = xml.etree.ElementTree.parse(filepath).getroot()
     return sorted([x.attrib['id'] for x in e.findall(".//*[@id='LAYER_EFO']/*") if re.match('[A-Z]+_\w+', x.attrib['id'] )])
 
-ids_for_svgs = {filename: idsInFile(filename) for filename in svgsFound}
+ids_for_svgs = {filename: idsInFile(os.path.join(dirpath,filename)) for (dirpath,filename) in svgsFound}
 
 with open(TARGET,'w') as target_file:
     json.dump(ids_for_svgs, target_file, indent=2)
