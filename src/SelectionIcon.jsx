cf4ef1822979610b9a058ef0a4c0a59ccbd95be6
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-01-27 01:54:29 +0000

    ES6-ify; idiomatic React as of React 15; remove shouldComponentUpdate: let React do its thing

--- src/SelectionIcon.jsx
+++ src/SelectionIcon.jsx
@@ -1,31 +1,28 @@
-const React = require(`react`);
-const ResolvePathToIcon = require(`./imagesAvailable.js`).ResolvePathToIcon;
-require(`./SelectionIcon.less`);
+import React from 'react';
+import {ResolvePathToIcon} from './imagesAvailable.js';
+import './SelectionIcon.less';
 
-const SelectionIcon = React.createClass({
-    propTypes: {
-        pathToFolderWithBundledResources: React.PropTypes.string.isRequired,
-        anatomogramType: React.PropTypes.oneOf([`brain`,`female`,`male`,`whole_plant`,`flower_parts`]).isRequired,
-        selected: React.PropTypes.bool.isRequired,
-        onClick: React.PropTypes.func.isRequired
-    },
+class SelectionIcon extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
         return (
             <img className={"selection-icon"} onClick={this.props.onClick}
-                 src={
-                     ResolvePathToIcon(
+                 src={ResolvePathToIcon(
                          this.props.pathToFolderWithBundledResources,
                          this.props.anatomogramType,
-                         this.props.selected
-                     )
-                 }/>
+                         this.props.selected)} />
         );
-    },
-
-    shouldComponentUpdate(nextProps) {
-        return this.props.selected !== nextProps.selected;
     }
-});
+}
 
-module.exports = SelectionIcon;
+SelectionIcon.propTypes = {
+    pathToFolderWithBundledResources: React.PropTypes.string.isRequired,
+    anatomogramType: React.PropTypes.oneOf([`brain`,`female`,`male`,`whole_plant`,`flower_parts`]).isRequired,
+    selected: React.PropTypes.bool.isRequired,
+    onClick: React.PropTypes.func.isRequired
+};
+
+export default SelectionIcon;
