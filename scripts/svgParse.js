eebd0a67342ac04bd200f94b06e0481cec505f67
wbazant <wbazant@ebi.ac.uk>
2017-07-27 16:58:58 +0100

    Write to stdout. If the script has nothing to say it shouldn't say it. :)

--- scripts/svgParse.js
+++ scripts/svgParse.js
@@ -8,37 +8,33 @@ const parseSvgFile = (filename) => ({
 
 if (process.argv.length < 3) {
   process.stdout.write(`svgMetadataParser: no input files\n\n`)
-  process.stdout.write(`usage: node svgMetadataParser.js <source> [destination]\n\n`)
+  process.stdout.write(`usage: node svgMetadataParser.js <source> \n\n`)
   process.stdout.write(`Source can be a SVG file or a directory containing one or more SVG files.\n`)
   process.stdout.write(`If source is set to "-" input is read from stdin.\n`)
   process.exit(1)
 }
 
-const outStream = process.argv[3] ? fs.createWriteStream(process.argv[3]) : process.stdout
-
 if (process.argv[2] === `-`) {
   const chunks = []
   process.stdin.on(`data`, (chunk) => { chunks.push(chunk) })
   process.stdin.on(`end`, () => {
-    outStream.write(JSON.stringify(parseSvg(Buffer.concat(chunks).toString()), null, 2))
+    process.stdout.write(JSON.stringify(parseSvg(Buffer.concat(chunks).toString()), null, 2))
   })
 }
 else {
   const inPathStats = fs.lstatSync(process.argv[2])
 
   if (inPathStats.isFile()) {
-     outStream.write(JSON.stringify(parseSvgFile(process.argv[2]), null, 2))
+     process.stdout.write(JSON.stringify(parseSvgFile(process.argv[2]), null, 2))
 
   } else if (inPathStats.isDirectory()) {
-    outStream.write(
+    process.stdout.write(
       JSON.stringify(
         fs.readdirSync(process.argv[2])
           .filter((filename) => path.extname(filename) === `.svg`)
           .map((filename) => path.resolve(process.argv[2], filename))
           .reduce((acc, pathToFile) => {
-            process.stdout.write(`Processing ${pathToFile}...`)
             const parsed = parseSvgFile(pathToFile)
-            process.stdout.write(` done\n`)
             return Object.assign(acc, parsed)
           }, {}),
         null, 2))
