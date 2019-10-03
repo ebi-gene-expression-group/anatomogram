a3df9580c525a9ee454c851881e77bfbf6663426
amunozpomer <amunoz@ebi.ac.uk>
2019-10-03 13:47:28 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require(`path`)
-const CleanWebpackPlugin = require(`clean-webpack-plugin`)
+const { CleanWebpackPlugin } = require(`clean-webpack-plugin`)
 
 const commonPublicPath = `/dist/`
 const vendorsBundleName = `vendors`
